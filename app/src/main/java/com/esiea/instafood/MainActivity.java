c35d2d11d5441b2a749dd8dcab502a5b7878cb72
Gabrielle <tchuenboukomgu@et.esiea.fr>
2018-05-18 14:32:42 +0200

    Initial commit

---
+++ app/src/main/java/com/esiea/instafood/MainActivity.java
@@ -0,0 +1,13 @@
+package com.esiea.instafood;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
